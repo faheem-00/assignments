f3db35e877550235e0d7216caea457ea0a078396
faheem-00 <163834996+faheem-00@users.noreply.github.com>
2024-10-26 10:09:35 +0530

    Update Alphabet or not.py

--- Alphabet or not.py
+++ Alphabet or not.py
@@ -3,5 +3,9 @@ char= input("Enter the Alphabet:")
 if char=='a' or char=='b' or char=='c' or char=='d' or char=='e' or char=='f' or char=='g' or char=='h' or char=='i' or char=='j' or char=='k'or char=='l' or char=='m' or char=='n' or char=='o' or char=='p' or char=='q' or char=='r' or char=='s' or char=='t' or char=='u' or char=='v' or char=='w' or char=='x' or char=='y' or char=='z':
     
     print('The given Character {} is an Alphabet'.format(char))
+    
+elif char=='A' or char=='B' or char=='C' or char=='D' or char=='E' or char=='F' or char=='G' or char=='H' or char=='I' or char=='J' or char=='K'or char=='L' or char=='M' or char=='N' or char=='O' or char=='P' or char=='Q' or char=='R' or char=='S' or char=='T' or char=='U' or char=='V' or char=='W' or char=='X' or char=='Y' or char=='Z':
+
+    print('The given Character {} is an Alphabet'.format(char))
 else:
     print('The given Character {} is not an Alphabet'.format(char))
